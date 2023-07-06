40f632def1b0f6b88a592cfd3e068c38d86b55b5
rakib333 <smrakib333@gmail.com>
2023-07-07 03:39:32 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+import './App.css'
+import Home from "./components/MainLayout/Home";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Home></Home>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/MainLayout/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is home</h2>
+        </div>
+    );
+};
+
+export default Home;
