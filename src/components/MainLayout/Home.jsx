d3e173204455f9e625210fcbb901b3b51df8b161
rakib333 <smrakib333@gmail.com>
2023-07-07 03:48:44 +0600

    added

--- src/components/MainLayout/Home.jsx
+++ src/components/MainLayout/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css';
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <h2>This is home layout</h2>
         </div>
     );
 };
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import './Products.css'
+
+const Products = () => {
+    return (
+        <div>
+            <h3>This is products container</h3>
+        </div>
+    );
+};
+
+export default Products;
